6982546762f8be041aaceab305b8a82dfec7c5f1
unknown <suzu.chao@gmail.com>
2020-04-18 15:56:33 +0900

    first add

---
+++ master/LoginForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace master
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.Idtxt = new System.Windows.Forms.TextBox();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(232, 133);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 0;
+            this.btnlogin.Text = "ログイン";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // Idtxt
+            // 
+            this.Idtxt.Location = new System.Drawing.Point(105, 46);
+            this.Idtxt.Name = "Idtxt";
+            this.Idtxt.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Idtxt.Size = new System.Drawing.Size(145, 19);
+            this.Idtxt.TabIndex = 1;
+            this.Idtxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passtxt
+            // 
+            this.passtxt.Location = new System.Drawing.Point(105, 96);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.passtxt.Size = new System.Drawing.Size(145, 19);
+            this.passtxt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "社員ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "パスワード";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 179);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.Idtxt);
+            this.Controls.Add(this.btnlogin);
+            this.Name = "LoginForm";
+            this.Text = "ログイン";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox Idtxt;
+        private System.Windows.Forms.TextBox passtxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
